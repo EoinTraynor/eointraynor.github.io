94e8a9f90b031f51ddc125948c3038984621a158
Eoin Traynor <eointraynor1@gmail.com>
2019-06-30 13:47:54 +0100

    init gatsby

---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = () => (
+  <div>
+    <h1>Hi,</h1>
+    <p>My name is Eoin, I'm a software developer living in Dublin. I work for HubSpot</p>    
+  </div>
+)
+
+export default About
