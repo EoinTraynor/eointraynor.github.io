099a5b95d43d323a482598294c1524507aec290e
Eoin Traynor <etraynor@hubspot.com>
2021-12-16 16:11:48 +0100

    Uncommited type

---
+++ types/extendedPost.ts
@@ -0,0 +1,15 @@
+import Author from './author';
+
+type ExtendedPostType = {
+  slug: string
+  title: string
+  date: string
+  coverImage: string
+  author: Author
+  excerpt: string
+  ogImage: {
+    url: string
+  }
+  content: string
+}
+export default ExtendedPostType
