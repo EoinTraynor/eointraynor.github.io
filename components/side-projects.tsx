754a212e49a445645239c2567883d81bab74188e
Eoin Traynor <etraynor@hubspot.com>
2022-04-06 15:56:58 +0200

    Remove dynamic pages, add section comonents

---
+++ components/side-projects.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react';
+
+const SideProjects: FunctionComponent = () => {
+  return <div className="container mx-auto px-5">About Me.</div>;
+};
+
+export default SideProjects;
