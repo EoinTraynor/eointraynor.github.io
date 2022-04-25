29e3c8623fb3439d0984537e7b800d574c98c6ae
Eoin Traynor <etraynor@hubspot.com>
2022-04-26 00:19:53 +0200

    About Section

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,27 +1,33 @@
-import { userData } from '@constants';
 import Image from 'next/image';
 import { FunctionComponent } from 'react';
 import profileImage from '../public/assets/profile/profile.jpg';
-import WorkExperience from './WorkExperience';
 
 const AboutMe: FunctionComponent = () => {
   return (
     <div id="about-me" className="mx-auto">
-      <div className="flex py-8">
-        <div>
-          <h2 className="text-2xl">⚡ About Me.</h2>
-          <p>{userData.bio}</p>
-        </div>
-        <div className="max-w-md px-8">
-          <Image
-            src={profileImage}
-            alt="Profile Image"
-            width={640}
-            height={461}
-          />
+      <div className="py-8">
+        <h2 className="text-2xl py-8">⚡ About Me.</h2>
+        <div className="flex flex-col items-center">
+          <div className="px-8 max-w-xs">
+            <Image
+              className="rounded-full max-w-xs"
+              src={profileImage}
+              alt="Profile Image"
+            />
+          </div>
+          <ul className="mt-4">
+            <li>🖥 I’m a developer.</li>
+            <li>💼 I work at HubSpot as a Senior Software Engineer.</li>
+            <li>📍 I&apos;ve recently moved to Berlin</li>
+            <li>🛠️ I primarily work with ReactJS, TypeScript, NodeJS & AWS</li>
+            <li>🌱 I&apos;m currently learning to speak German</li>
+            <li>📫 Feel free to email or connect with me</li>
+            <li>
+              ⚡️ Fun fact: You can install me <code>npx eointraynor</code>
+            </li>
+          </ul>
         </div>
       </div>
-      <WorkExperience />
     </div>
   );
 };
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,12 @@
 import AboutMe from '@components/AboutMe';
 import PageTemplate from '@components/PageTemplate';
+import WorkExperience from '@components/WorkExperience';
 
 const About = () => {
   return (
     <PageTemplate>
       <AboutMe />
+      <WorkExperience />
     </PageTemplate>
   );
 };
